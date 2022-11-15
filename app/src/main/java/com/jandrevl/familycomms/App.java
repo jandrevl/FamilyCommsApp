4f2e9cfc7c80a01bf833cd35ec8daa182334a9d0
Joao Lopes <jandrevl@gmail.com>
2022-11-15 13:58:05 +0000

    First Commit. Parse Server connected to the app

---
+++ app/src/main/java/com/jandrevl/familycomms/App.java
@@ -0,0 +1,48 @@
+package com.jandrevl.familycomms;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import android.app.Application;
+import android.util.Log;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("myappID")
+                // if defined
+                .clientKey("DCqfyAFdW10D")
+                .server("http://18.188.58.143/parse/")
+                .build()
+        );
+
+        ParseObject object = new ParseObject("ExampleObject");
+        object.put("myNumber", "123");
+        object.put("myString", "rob");
+
+        object.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException ex) {
+                if (ex == null) {
+                    Log.i("Parse Result", "Successful!");
+                } else {
+                    Log.i("Parse Result", "Failed" + ex.toString());
+                }
+            }
+        });
+
+
+        ParseUser.enableAutomaticUser();
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}
