3cfe004c2ff764511271351571e1a2a2ee2f2d9b
Joao Lopes <jandrevl@gmail.com>
2022-11-16 13:28:51 +0000

    Initial Layout screen created

--- app/src/main/java/com/jandrevl/familycomms/App.java
+++ app/src/main/java/com/jandrevl/familycomms/App.java
@@ -22,23 +22,23 @@ public class App extends Application {
                 .build()
         );
 
-        ParseObject object = new ParseObject("ExampleObject");
-        object.put("myNumber", "123");
-        object.put("myString", "rob");
-
-        object.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException ex) {
-                if (ex == null) {
-                    Log.i("Parse Result", "Successful!");
-                } else {
-                    Log.i("Parse Result", "Failed" + ex.toString());
-                }
-            }
-        });
-
-
-        ParseUser.enableAutomaticUser();
+//        ParseObject object = new ParseObject("ExampleObject");
+//        object.put("myNumber", "123");
+//        object.put("myString", "rob");
+//
+//        object.saveInBackground(new SaveCallback() {
+//            @Override
+//            public void done(ParseException ex) {
+//                if (ex == null) {
+//                    Log.i("Parse Result", "Successful!");
+//                } else {
+//                    Log.i("Parse Result", "Failed" + ex.toString());
+//                }
+//            }
+//        });
+//
+//
+//        ParseUser.enableAutomaticUser();
 
         ParseACL defaultACL = new ParseACL();
         defaultACL.setPublicReadAccess(true);
