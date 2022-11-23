8aab2b96ed2f6e29ad26af926cb753510b81f33b
Joao Lopes <jandrevl@gmail.com>
2022-11-23 23:20:31 +0000

    Managed to obtain userList from ParseQuery. Begin to work on Recycler View

--- app/src/main/java/com/jandrevl/familycomms/UsersListActivity.java
+++ app/src/main/java/com/jandrevl/familycomms/UsersListActivity.java
@@ -6,31 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsersListActivity extends AppCompatActivity {
 
     ParseUser currentUser;
     Intent intent;
-
     boolean doubleBackToExitPressedOnce = false;
+    ArrayList<String> usersList;
 
     @Override
     public void onBackPressed() {
         if (doubleBackToExitPressedOnce) {
             super.onBackPressed();
-//            return;
             finishAffinity();
-
         }
 
         this.doubleBackToExitPressedOnce = true;
         Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
 
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-
             @Override
             public void run() {
                 doubleBackToExitPressedOnce=false;
@@ -45,7 +51,6 @@ public class UsersListActivity extends AppCompatActivity {
 
         intent = getIntent();
 
-
         currentUser = ParseUser.getCurrentUser();
         if (currentUser != null) {
             setTitle("Olá " + currentUser.getUsername());
@@ -54,7 +59,33 @@ public class UsersListActivity extends AppCompatActivity {
             Toast.makeText(this, "Sem sessão iniciada", Toast.LENGTH_SHORT).show();
         }
 
+        usersList = new ArrayList<String>();
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("_User");
+        query.whereNotEqualTo("username", currentUser.getUsername());
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> parseUsers, ParseException e) {
+                if(e == null) {
+                    for(ParseObject parseUser : parseUsers) {
+                        Log.i("User", parseUser.getString("username"));
+                        usersList.add(parseUser.getString("username"));
+                    }
+                    Log.i("Users List", usersList.toString());
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
 
+
+
+    }
+
+    public void changeUser(View view) {
+        ParseUser.logOut();
+        Intent i=new Intent(this, MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
     }
 
 
